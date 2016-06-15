46e7dc89c371622b813bce76c06f37a397f0ccfc
George G <ggogarten@gmail.com>
2016-06-15 16:04:20 -0400

    simple timer setup

--- Navigation Bar/ViewController.swift
+++ Navigation Bar/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var time = 0
+   
+    func result() {
+    
+        time = time + 1
+        
+        print(time)
+    
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var timer = NSTimer()
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
